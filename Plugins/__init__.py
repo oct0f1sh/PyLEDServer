9a110a4bc4b6f83224004aef99073767485a0162
Duncan <buzzbuzz.macdonald@gmail.com>
2018-05-30 10:13:49 -0700

    added plugins directory

--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -1,5 +1,4 @@
-import os
-import importlib
-import pyfile_exists
-__all__ = [importlib.import_module('.%s' % filename, __package__) for filename in [os.path.splitext(i)[0] for i in os.listdir(os.path.dirname(__file__)) if os.path.splitext(i)[1] in pyfile_exists] if not filename.startswith('__')]
-del os, importlib, pyfile_exists
+import os, glob
+
+modules = glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
+__all__ = [os.path.basename(f)[:-3] for f in modules if not f.endswith("__init__.py")]
