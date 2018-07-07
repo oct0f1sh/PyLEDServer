74f770f3393505410211ff4043546cf04ace61ad
Duncan <buzzbuzz.macdonald@gmail.com>
2018-07-06 22:16:21 -0700

    adding moving dot effects

--- Plugins/movingdot.py
+++ Plugins/movingdot.py
@@ -95,7 +95,7 @@ class MovingDotThread(threading.Thread):
 
         bulge = []
 
-        for i in reversed(range(1, num_points * 100, 100)):
+        for i in reversed(range(1, num_points * 10, 10)):
             bulge.append(Color(self.r / i ** 3, self.g / i ** 3, self.b / i ** 3))
 
         bulge.extend(bulge[::-1])
