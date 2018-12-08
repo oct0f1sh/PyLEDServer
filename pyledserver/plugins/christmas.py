dab6911c7877a4277d393eadfb041a3438d165a9
Duncan <buzzbuzz.macdonald@gmail.com>
2018-12-07 16:02:30 -0800

    fixed import

--- pyledserver/plugins/christmas.py
+++ pyledserver/plugins/christmas.py
@@ -48,5 +48,5 @@ class ChristmasThread(threading.Thread):
 
             odd = not odd
             self.led_strip.show()
-            time.sleep(self.duration)
+            sleep(self.duration)
                 
